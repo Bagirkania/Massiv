8c374981972d4821eb5e2b4b6fd5e9d294ee5fc8
Bagirka <bruna07@mail.ru>
2022-09-22 15:07:43 +0500

    Massiv_001

---
+++ Massiv/Massiv_001/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите трехзначное число: ");
+    while(true)
+    {
+    string input = Console.ReadLine();
+        if(!input.Equals("exit"))
+        Console.WriteLine("{0}->{1}",input, input[1]);
+        else
+            break;
+    }
+
+
