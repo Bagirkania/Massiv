0879ea9d60112618aef13d2ac540c058881d33be
Bagirka <bruna07@mail.ru>
2022-09-22 17:25:38 +0500

    Доработала

---
+++ Massiv/Massiv_002/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+string numberTxt = Convert.ToString(number);
+if (numberTxt.Length > 2){
+  Console.WriteLine("третья цифра -> " + numberTxt[2]);
+}
+else {
+  Console.WriteLine("-> третьей цифры нет");
+}
+
